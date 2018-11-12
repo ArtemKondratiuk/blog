a348c1f06b6af3bab5669b50a67eb11585f45f67
ArtemKondratyuk <artem.kondratyuk.1995@gmail.com>
2018-11-12 08:30:31 +0200

    regiser user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\RegisterType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/registration")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+}
