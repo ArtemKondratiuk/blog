2beed1bfe8a7a6502534d40126258f5c974b0645
ArtemKondratyuk <artem.kondratyuk.1995@gmail.com>
2018-12-13 08:09:02 +0200

    user fixture and role bloger

---
+++ src/Controller/BloggerController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Form\ArticleType;
+use App\Entity\Article;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+/**
+ * @Route("/bloger" )
+ * @Security("has_role('ROLE_BLOGER')")
+ */
+class BloggerController extends Controller
+{
+    /**
+     * @Route("/", name="bloger")
+     */
+    public function bloger(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('bloger');
+        }
+
+        return $this->render('bloger/bloger.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+            ]);
+    }
+}
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,7 +5,6 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\User;
-use App\Entity\UserLike;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
@@ -19,16 +18,33 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $encodedPassword = $this->passwordEncoder->encodePassword($user, '123123');
-        $user
+        $user1 = new User();
+        $encodedPassword = $this->passwordEncoder->encodePassword($user1, '123123');
+        $user1
             ->setFirstName('admin')
             ->setLastName('admin')
             ->setEmail('admin@gmail.com')
             ->setPassword($encodedPassword)
             ->setRoles(['ROLE_ADMIN']);
-
-        $manager->persist($user);
+        $manager->persist($user1);
+        $user2 = new User();
+        $encodedPassword = $this->passwordEncoder->encodePassword($user2, '123123');
+        $user2
+            ->setFirstName('reader')
+            ->setLastName('reader')
+            ->setEmail('reader@gmail.com')
+            ->setPassword($encodedPassword)
+            ->setRoles(['ROLE_READER']);
+        $manager->persist($user2);
+        $user3 = new User();
+        $encodedPassword = $this->passwordEncoder->encodePassword($user3, '123123');
+        $user3
+            ->setFirstName('bloger')
+            ->setLastName('bloger')
+            ->setEmail('bloger@gmail.com')
+            ->setPassword($encodedPassword)
+            ->setRoles(['ROLE_BLOGER']);
+        $manager->persist($user3);
 
         $manager->flush();
     }
